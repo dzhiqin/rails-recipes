a741e82bd59d179dec512a6ce5eef957e3c4367c
dzhiqin <dzhiqin@126.com>
2019-06-29 10:38:02 +0800

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:dzhiqin/rails-recipes.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/ruby/rails-recipes"
+set :deploy_to, "/home/ubuntu/rails-recipes"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
